6a3e8d61e774e004d13a46738eb185335655fc22
ANSH KUMAR <anshkumar1399@gmail.com>
2024-10-02 23:57:43 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Python Developer", "Database Creator"],
+    typeSpeed: 100,     
+    backSpeed: 100,     
+    backDelay: 1000,  
+    loop: true      
+});
+
+
+
